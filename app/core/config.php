86735c23d697b1a3aa2bc794ede9f87c4f12c79b
anthony <anythony20@gmail.com>
2025-06-04 20:27:22 +0000

    Update DB config

--- app/core/config.php
+++ app/core/config.php
@@ -14,8 +14,8 @@ define('LOGS', ROOT . DS . 'logs');
 define('FILES', ROOT . DS. 'files');
 
 // ---------------------  NEW DATABASE TABLE -------------------------
-define('DB_HOST',         'xxw.h.filess.io');
-define('DB_USER',         '4806_sentenceby'); 
+define('DB_HOST',         'lymrw.h.filess.io');
+define('DB_USER',         'cosc4806_harderwall'); 
 define('DB_PASS',         $_ENV['DB_PASS']);
-define('DB_DATABASE',     '4806_sentenceby');
-define('DB_PORT',         '3305');
+define('DB_DATABASE',     'cosc4806_harderwall');
+define('DB_PORT',         '61000');
