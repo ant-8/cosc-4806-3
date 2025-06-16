80e0b0e9072edf4e041f53a6f1fc3889df1a7e47
anthony <anythony20@gmail.com>
2025-06-16 17:22:00 +0000

    Implement account creation in user model

--- app/models/User.php
+++ app/models/User.php
@@ -2,49 +2,63 @@
 
 class User {
 
-    public $username;
-    public $password;
-    public $auth = false;
+  public $username;
+  public $password;
+  public $auth = false;
 
-    public function __construct() {
+  public function __construct() {
         
-    }
+  }
 
-    public function test () {
-      $db = db_connect();
-      $statement = $db->prepare("select * from users;");
-      $statement->execute();
-      $rows = $statement->fetch(PDO::FETCH_ASSOC);
-      return $rows;
-    }
+  public function test () {
+    $db = db_connect();
+    $statement = $db->prepare("select * from users;");
+    $statement->execute();
+    $rows = $statement->fetch(PDO::FETCH_ASSOC);
+    return $rows;
+  }
 
-    public function authenticate($username, $password) {
-        /*
-         * if username and password good then
-         * $this->auth = true;
-         */
-		$username = strtolower($username);
-		$db = db_connect();
-        $statement = $db->prepare("select * from users WHERE username = :name;");
-        $statement->bindValue(':name', $username);
-        $statement->execute();
-        $rows = $statement->fetch(PDO::FETCH_ASSOC);
+  public function authenticate($username, $password) {
+    /*
+     * if username and password good then
+     * $this->auth = true;
+    */
+	$username = strtolower($username);
+	$db = db_connect();
+    $statement = $db->prepare("select * from users WHERE username = :name;");
+    $statement->bindValue(':name', $username);
+    $statement->execute();
+    $rows = $statement->fetch(PDO::FETCH_ASSOC);
 		
-		if (password_verify($password, $rows['password'])) {
+	if (password_verify($password, $rows['password'])) {
 			$_SESSION['auth'] = 1;
 			$_SESSION['username'] = ucwords($username);
 			unset($_SESSION['failedAuth']);
 			header('Location: /home');
 			die;
+	} else {
+		if(isset($_SESSION['failedAuth'])) {
+			$_SESSION['failedAuth'] ++; //increment
 		} else {
-			if(isset($_SESSION['failedAuth'])) {
-				$_SESSION['failedAuth'] ++; //increment
-			} else {
-				$_SESSION['failedAuth'] = 1;
-			}
-			header('Location: /login');
-			die;
+			$_SESSION['failedAuth'] = 1;
 		}
-    }
+		header('Location: /login');
+		die;
+	}
+  }
+
+  public function create_user($username, $password) {
+      $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+      $db = db_connect();
+      $statement = $db->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
+      $success = $statement->execute([
+          ':username' => $username,
+          ':password' => $hashed_password
+      ]);
+
+      return ['success' => $success, 'message' => $success ? 'Account created' : 'Failed to create account.'];
+  }
+
 
 }
